abd26ce67bdcb0c917bdf968489c88273e425f32
Kevin Hu <kevindhu16@gmail.com>
2018-01-26 23:08:52 -0800

    redirected logs

--- logger.py
+++ logger.py
@@ -9,19 +9,27 @@ class Logger:
     def log(self, text):
         print(text)
         date = datetime.datetime.now()
+        directory = "logs/{0}-{1}-{2}.txt".format(str(date.month), str(date.day), str(date.hour))
+        directory2 = None
+
+
         if self.name != "":
             if self.name.endswith("receipt"):
-                dir = "logs/{0}-{1}/receipts/{2}.txt".format(str(date.month), str(date.day), self.name)
+                directory2 = "logs/{0}-{1}/receipts/{2}.txt".format(str(date.month), str(date.day), self.name)
                 file = "logs/{0}-{1}/receipts/".format(str(date.month), str(date.day))
             else:
-                dir = "logs/{0}-{1}/{2}.txt".format(str(date.month), str(date.day), self.name)
+                directory2 = "logs/{0}-{1}/{2}.txt".format(str(date.month), str(date.day), self.name)
                 file = "logs/{0}-{1}/".format(str(date.month), str(date.day))
             if not os.path.exists(file):
                 os.makedirs(file)
-        else:
-            dir = "logs/{0}-{1}-{2}.txt".format(str(date.month), str(date.day), str(date.hour))
+
         timestamp = datetime.datetime.now()
 
-        with open(dir, 'a') as f:
+        with open(directory, 'a') as f:
             f.write("{0}: {1}\n".format(timestamp, text))
             f.close()
+
+        if directory2:
+            with open(directory2, 'a') as f:
+                f.write("{0}: {1}\n".format(timestamp, text))
+                f.close()
